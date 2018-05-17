ecf91744365f4b30cf9ba8d7caf3ad50aa263bcc
Abhilash Mishra <abhilash.mishra@swiggy.in>
2018-05-17 11:04:32 +0530

    fixed uptracked files

--- app/src/main/java/com/abhilashmishra/slither/MainActivity.kt
+++ app/src/main/java/com/abhilashmishra/slither/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        createLithoView()
+        createLithoView();
 
     }
 
